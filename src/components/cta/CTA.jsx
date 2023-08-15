8e938e855ba2baca82ef2c40b77de029d8cc0ff9
CoderRoy05 <124418556+CoderRoy05@users.noreply.github.com>
2023-08-15 01:46:13 +0100

    Update CTA.jsx

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -15,7 +15,7 @@ const CTA = () => (
       <p>started with no requiremnets</p>
       <div className="gpt3__more_on">
         <p>actually happening right now</p>
-        <p>although</p>
+        <p>although, the main events</p>
       </div>
     </div>
   </div>
