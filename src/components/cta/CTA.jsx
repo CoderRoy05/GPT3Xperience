7724e0609716ddf4ea41b4806cb615b55fa830fe
CoderRoy05 <124418556+CoderRoy05@users.noreply.github.com>
2023-08-17 01:10:19 +0100

    Update CTA.jsx

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -13,7 +13,7 @@ const CTA = () => (
       <button type="main">Update Access</button>
       <p>Always start with something new and fresh</p>
       <p>Our second heck start from the next min</p>
-      <p>started with no requiremnets, unless you are with the Tickets (noted) only for the serious type</p>
+      <p>started with no requiremnets, unless you are with the Tickets (note) only for the serious type</p>
       <div className="double__mark-sign">in space</div>
       <div className="gpt3__more_on">
         <p>actually happening right now</p>
