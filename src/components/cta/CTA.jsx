02d6524b3ad274832c6dca81654444264144fee0
CoderRoy05 <124418556+CoderRoy05@users.noreply.github.com>
2023-08-18 02:24:31 +0100

    Update CTA.jsx

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -14,7 +14,7 @@ const CTA = () => (
       <p>Always start with something new and fresh</p>
       <p>Our second heck start from the next min</p>
       <p>started with no requiremnets, unless you are with the Tickets (note) only for the serious type</p>
-      <div className="double__mark-sign">beside</div>
+      <div className="double__mark-sign">beside eachother</div>
       <div className="gpt3__more_on">
         <p>actually happening right now</p>
         <p>although, the main events interaction also occur</p>
